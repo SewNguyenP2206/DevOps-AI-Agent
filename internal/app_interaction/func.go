6553b276f515b953d80f859f8f3b8b17acb77434
SewNguyenP2206 <151114359+SewNguyenP2206@users.noreply.github.com>
2025-12-13 21:07:37 +0700

    Update func.go

--- internal/app_interaction/func.go
+++ internal/app_interaction/func.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-// Handle
+// Handl
 func HandleAppOpenRequest(input string, memory *[]string) {
 	prompt := fmt.Sprintf(`
 You are an AI assistant. The user wants to open an application.
